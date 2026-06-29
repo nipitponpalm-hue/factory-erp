d3333ef1db4fe801d90ac7afd4785453b0c1be25
nipitponpalm-hue <nipitpon.palm@gmail.com>
2026-06-29 14:24:40 +0700

    Update Supabase credentials to new project

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://dsnlzppbannlkmixksco.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_L5K6XA0aQyRn3QFH_Woc_g_QqK0eFvd";
+const SUPABASE_URL = "https://zccwgfwiwrobdqyerjmy.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_Cj_5VZ3QWWUu-vOQbZbqDg_2a9w8t4M";
 
 const db = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
